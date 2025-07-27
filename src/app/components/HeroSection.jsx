f71843e5f263406f93ba6da7d3161600bc5c6bed
Brittany Holmes <Brittany.Holmes@vanarts.com>
2025-07-27 17:04:43 -0600

    change text to white

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const HeroSection = () => {
   return (
     <div className='h-screen flex flex-col justify-end items-center gap-3 backgroundImage'>
-         <h1 className='text-5xl font-bold' >BRITTANY HOLMES</h1>
+         <h1 className='text-5xl font-bold text-white' >BRITTANY HOLMES</h1>
          <div className='max-w-1/4 text-center mb-20'>
-            <p>I create custom visuals and websites that reflect the passion behind your work</p>
+            <p className='text-white'>I create custom visuals and websites that reflect the passion behind your work</p>
          </div>
     </div>
   )
