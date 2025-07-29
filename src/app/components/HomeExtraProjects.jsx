0c66e6596e79988e00c233d501483f9340a0a2dd
solacedesign <Brittany.holmes@vanarts.com>
2025-07-29 13:09:10 -0700

    added extra projects section

--- src/app/components/HomeExtraProjects.jsx
+++ src/app/components/HomeExtraProjects.jsx
@@ -24,7 +24,6 @@ const HomeExtraProjects = () => {
             
             </motion.div>
         </div>
-
         <div className='border-t border-t-neutral-300 border-b border-b-neutral-300 py-10 flex justify-between relative'>
             <h2 className='text-3xl font-bold text-black'>Project Title</h2>
             <p className='text-black text-lg'>This will be a short description</p>
@@ -47,7 +46,17 @@ const HomeExtraProjects = () => {
             
             </motion.div>
         </div>
+        <div className='border-t border-t-neutral-300 border-b border-b-neutral-300 py-10 flex justify-between relative'>
+            <h2 className='text-3xl font-bold text-black'>Project Title</h2>
+            <p className='text-black text-lg'>This will be a short description</p>
 
+            <motion.div 
+            initial={{opacity: 0}}
+            whileHover={{opacity: 1, rotate: 10}}
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] aspect-video bg-neutral-400 shadow-xl z-10">
+            
+            </motion.div>
+        </div>
         <div className='border-t border-t-neutral-300 border-b border-b-neutral-300 py-10 flex justify-between relative'>
             <h2 className='text-3xl font-bold text-black'>Project Title</h2>
             <p className='text-black text-lg'>This will be a short description</p>
